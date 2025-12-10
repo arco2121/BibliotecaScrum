6f520a68f67d12374ebd58d479f0185397152d2b
Marco Colombara <colombaramarco21@gmail.com>
2025-12-10 13:05:11 +0100

    First implementation of signUp

--- pages/signup.php
+++ pages/signup.php
@@ -5,7 +5,7 @@ session_start();
 
 // Redirect se già loggato
 if (isset($_SESSION['logged']) && $_SESSION['logged'] === true) {
-    header("Location: ./");
+    header("Location: /");
     exit;
 }
 
@@ -33,8 +33,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if(empty($datiDaCodice)) {
             $status = "Codice Fiscale non definito";
         } else {
-            $nome = $datiDaCodice['nome'] ?? '';
-            $cognome = $daCodiceFiscale['cognome'] ?? '';
+            $nome = $_POST['nome'] ?? '';
+            $cognome = $_POST['cognome'] ?? '';
             $data_nascita = $daCodiceFiscale['data_nascita'] ?? '';
             $comune_nascita = $daCodiceFiscale['comune_nascita'] ?? '';
             $sesso = $daCodiceFiscale['sesso'] ?? '';
@@ -44,8 +44,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     //Inserimento nel database (query da modificare se necessario)
     if($status != '') {
-        $insert_string = "INSERT INTO users (nome, cognome, comune_nascita, data_nascita, sesso, codice_fiscale,email, password) VALUES (:nome,:cognome,:comune,:data,:sesso,:codice_fiscale,:email, :password),?,?)";
-        $stmt = getInstance().getConnection()->prepare($insert_string);
+        $insert_string = "INSERT INTO users (nome, cognome, comune_nascita, data_nascita, sesso, codice_fiscale,email, password) VALUES (:nome,:cognome,:comune,:data,:sesso,:codice_fiscale,:email, :password)";
+        $stmt = $pdo->prepare($insert_string);
         $stmt->bindParam(":nome", $nome);
         $stmt->bindParam(":conome", $cognome);
         $stmt->bindParam(":data", $data_nascita);
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->bindParam(":email", $email);
         $stmt->bindParam(":password", $password);
         $result = $stmt->execute();
-        if($result) header("Location: ./login");
+        if($result) header("Location: /login");
         else $status = "Errore nell'inserimento dell'utente";
     }
 }
@@ -64,6 +64,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
 </head>
 <body>
@@ -74,6 +78,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php if ($status) : echo $status; ?>
     <h2>Registrati<?= $tipologia ?></h2>
     <form method="post">
+        <label for="nome">Nome:</label>
+        <input required type="text" id="nome" name="nome">
+        <label for="cognome">Cognome:</label>
+        <input required type="text" id="cognome" name="cognome">
         <?php if($registratiConCodice) { ?>
             <label for="codice_fiscale">Codice Fiscale:</label>
             <input required type="text" id="codice_fiscale" name="codice_fiscale">
@@ -82,10 +90,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <label for="password">Password:</label>
             <input required type="password" id="password" name="password">
         <?php } else { ?>
-            <label for="nome">Nome:</label>
-            <input required type="text" id="nome" name="nome">
-            <label for="cognome">Cognome:</label>
-            <input required type="text" id="cognome" name="cognome">
             <label for="comune_nascita">Comune di Nascita:</label>
             <input required type="text" id="comune_nascita" name="comune_nascita">
             <label for="data_nascita">Data di Nascita:</label>
@@ -108,9 +112,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </form>
 
     <?php if($registratiConCodice) { ?>
-        <a href='/signup.php/?conCodiceFiscale=true'>Non hai il codice fiscale?</a>
+        <a href='/signup/?conCodiceFiscale=true'>Non hai il codice fiscale?</a>
     <?php } else {?>
-        <a href='/signup.php/?conCodiceFiscale=false'>Hai il codice fiscale?</a>
+        <a href='/signup/?conCodiceFiscale=false'>Hai il codice fiscale?</a>
     <?php } ?>
 </div>
 
