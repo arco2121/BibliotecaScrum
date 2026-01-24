50b73d68c9a9c87407e0a2114774f2c4af3bb74e
Gianluca Grammatica <grammaticagianluca@gmail.com>
2026-01-24 11:04:01 +0100

    Elemento Timer

--- public/scripts/game_timer.js
+++ public/scripts/game_timer.js
@@ -4,11 +4,7 @@ window.onload = function () {
     var tens = 0;
     var appendTens = document.getElementById("tens")
     var appendSeconds = document.getElementById("seconds")
-    var buttonStart = document.getElementById('button-start');
-    var buttonStop = document.getElementById('button-stop');
-    var buttonReset = document.getElementById('button-reset');
     var Interval ;
-    let timerText = document.getElementById("timer_text")
 
     function timerInit () {
         clearInterval(Interval);
